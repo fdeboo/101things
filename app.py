eecab47d5c76d56317865bfe0986d701ca9afdfc
Frances <frances.deboo@gmail.com>
2020-05-20 13:33:01 +0100

    Installed initial dependencies and imports

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return "Hello World"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=true)
