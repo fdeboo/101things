c80fe2e48b0bfa8974448f5529ddaf43a9956b55
Frances <frances.deboo@gmail.com>
2020-05-20 16:43:40 +0100

    CreateLocationForm class, form and route created.

--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,13 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired, Length
 from app import mongo
+
+class CreateLocationForm(FlaskForm):
+    location = StringField('Location', validators=[InputRequired(), Length(min=2, max=25))]
+    submit = SubmitField('Add Location')
+
+    def validate_location(self, location):
+        place_name = mongo.db.cities.find_one({'location': location.data})
+        if place_name:
+            raise ValidationError('This location has already been created.')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from app.forms import CreateLocationForm
 from app import app, mongo
 
 
@@ -8,3 +9,20 @@ from app import app, mongo
 @app.route('/home')
 def index():
     return render_template('home.html')
+
+
+
+@app.route('/addlocation', methods['POST'])
+def add_location():
+    form = CreateLocationForm()
+    cities = mongo.db.cities
+    if form.validate_on_submit():
+        cities.insert(
+            {
+                'location':form.location.data,
+                'picture':form.location.data
+            }
+        )
+        location = form.location.data
+        return redirect(url_for('add_activity', location=location))
+    return render_template('addlocation', form=form)
