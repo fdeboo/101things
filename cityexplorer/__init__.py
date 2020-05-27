13bdd73438c2f0748cf40f9282653575cb2c070e
Frances <frances.deboo@gmail.com>
2020-05-27 12:26:30 +0100

    Created an instance of object class Mail

--- cityexplorer/__init__.py
+++ cityexplorer/__init__.py
@@ -12,6 +12,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 app.config['MONGO_URI'] = Config.MONGO_URI
+mail = Mail(app)
 mongo = PyMongo(app)
 
 from cityexplorer import routes
