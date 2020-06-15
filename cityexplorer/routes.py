fc6f2a3d6a72caebde35e1f1d202251567df2090
Frances <frances.deboo@gmail.com>
2020-06-16 00:56:27 +0100

    if statement moved from home route to template

--- cityexplorer/routes.py
+++ cityexplorer/routes.py
@@ -14,12 +14,10 @@ from cityexplorer.utils import send_reset_email
 def index():
     form = SearchLocationForm()
     cities = mongo.db.cities
-    searched=form.search.data
+    searched = form.search.data
     if form.validate_on_submit():
-        query = cities.find({'location': searched.title()})
-        if query.count() == 0:
-            return render_template('home.html', form=form, search=searched, title="Home")
-        return render_template('home.html', locations=query, form=form, title="Home")
+        query = cities.find({'location': form.search.data.title()})
+        return render_template('home.html', locations=query, searched=searched, form=form, title="Home")
     query = cities.find({})
     if cities.find({'thingsToDo': {'$exists': False}}):
         cities.delete_many({'thingsToDo': {'$exists': False}})
